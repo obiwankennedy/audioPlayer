926ab12ecf8b559a08a3d8f90d83e3a6858002bf
Renaud G. <renaud@rolisteam.org>
2014-05-30 17:46:21 +0200

    Update sortdialog.cpp

--- src/sortdialog.cpp
+++ src/sortdialog.cpp
@@ -1,6 +1,5 @@
 /***************************************************************************
  *   Copyright (C) 2007 by Renaud Guezennec   *
- *   renaud.guezennec@gmail.com   *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
