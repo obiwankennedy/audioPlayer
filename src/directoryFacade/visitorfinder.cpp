aa3e73dd0fdd9ef2e705bd7bc5bd762d40786797
Renaud G <renaud@rolisteam.org>
2014-09-14 02:30:18 +0200

    -all files are using the oggTagLib.

--- src/directoryFacade/visitorfinder.cpp
+++ src/directoryFacade/visitorfinder.cpp
@@ -18,6 +18,7 @@
  ***************************************************************************/
 #include "../directoryFacade/visitorfinder.h"
 #include <QDebug>
+
 VisitorFinder::VisitorFinder()
  : VisitorNode()
 {
