00b15d5f13105738737a5fa3ec24d106f67fd3ef
Renaud G <renaud@rolisteam.org>
2014-12-29 17:06:41 +0100

    -implementation of song.

--- src/data/song.cpp
+++ src/data/song.cpp
@@ -1,5 +1,102 @@
 #include "song.h"
 
 Song::Song()
+    : m_genre(-1),m_duration(-1),m_bitrate(-1),m_year(-1)
 {
+
+}
+// gets
+QString Song::getTitle() const
+{
+    return m_title;
+}
+
+QString Song::getArtistName() const
+{
+    return m_artistName;
+}
+QString Song::getAlbum() const
+{
+    return m_album;
+}
+QString Song::getComment() const
+{
+    return m_comment;
+}
+QString Song::getLyrics() const
+{
+    return m_lyrics;
+}
+
+QImage Song::getImage() const
+{
+    return m_image;
+}
+int Song::getYear() const
+{
+    return m_year;
+}
+int Song::getBitrate() const
+{
+    return  m_bitrate;
+}
+int Song::getGenre() const
+{
+    return m_genre;
+}
+int Song::getTracNumber() const
+{
+    return m_trackNumber;
+}
+int Song::getDuration() const
+{
+    return m_duration;
+}
+
+
+//sets
+void Song::setTitle(QString s)
+{
+    m_title=s;
+}
+void Song::setArtistName(QString s)
+{
+    m_artistName=s;
+}
+void Song::setAlbum(QString s)
+{
+    m_album =s;
+}
+void Song::setComment(QString s)
+{
+    m_comment = s;
+}
+void Song::setLyrics(QString s)
+{
+    m_lyrics = s;
+}
+
+void Song::setImage(QImage i)
+{
+    m_image = i;
+}
+void Song::setYear(int i)
+{
+    m_year = i;
+}
+void Song::setBitrate(int i)
+{
+    m_bitrate = i;
+}
+void Song::setGenre(int i)
+{
+    m_genre = i;
+}
+void Song::setTracNumber(int i)
+{
+    m_trackNumber = i;
+}
+void Song::setDuration(int i)
+{
+    m_duration = i;
 }
--- src/data/song.h
+++ src/data/song.h
@@ -1,10 +1,56 @@
 #ifndef SONG_H
 #define SONG_H
 
-class Song
+#include <QImage>
+#include "media.h"
+
+class Song : public Media
 {
 public:
     Song();
+
+    // gets
+    QString getTitle() const;
+    QString getArtistName() const;
+    QString getAlbum() const;
+    QString getComment() const;
+    QString getLyrics() const;
+
+    QImage getImage() const;
+    int getYear() const;
+    int getBitrate() const;
+    int getGenre() const;
+    int getTracNumber() const;
+    int getDuration() const;
+
+
+    //sets
+    void setTitle(QString);
+    void setArtistName(QString) ;
+    void setAlbum(QString) ;
+    void setComment(QString) ;
+    void setLyrics(QString) ;
+
+    void setImage(QImage) ;
+    void setYear(int) ;
+    void setBitrate(int) ;
+    void setGenre(int) ;
+    void setTracNumber(int) ;
+    void setDuration(int) ;
+
+private:
+    QString m_title;
+    QString m_artistName;
+    QString m_album;
+    QString m_comment;
+    QString m_lyrics;
+    QImage m_image;
+
+    int m_year;
+    int m_bitrate;
+    int m_genre;
+    int m_trackNumber;
+    int m_duration;
 };
 
 #endif // SONG_H
