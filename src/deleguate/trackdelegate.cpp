e42c5b07aea12fc4624f1b9ca1fe6716e75d8337
Renaud G <renaud@rolisteam.org>
2016-06-18 21:50:08 +0200

    fix indentation fix time reading

--- src/deleguate/trackdelegate.cpp
+++ src/deleguate/trackdelegate.cpp
@@ -11,87 +11,85 @@ TrackDelegate::TrackDelegate(int durationColumn,int YearColumn,int BitrateColumn
     this->GenreColumn=GenreColumn;
     played = -1;
     myPlaylist = Playlist::getInstance();
-   // former_played = -1;
+    // former_played = -1;
 }
 void TrackDelegate::setPlayed(int p)
 {
-	/*if(played!=-1)
-		former_played=played;*/
-		
-	played = p;
-	
-	/*if(former_played!=-1)
-		parent*/
+    /*if(played!=-1)
+        former_played=played;*/
+
+    played = p;
+
+    /*if(former_played!=-1)
+        parent*/
 }
 void TrackDelegate::paint(QPainter *painter,
                           const QStyleOptionViewItem &option,
                           const QModelIndex &index) const
 {
-	QStyleOptionViewItem myOption = option;
+    QStyleOptionViewItem myOption = option;
     myOption.displayAlignment = Qt::AlignRight | Qt::AlignVCenter;
-	 
-index.model()->data(index, Qt::DisplayRole).toInt();
-        if((*myPlaylist)[index.row()]->isReading())
-                        {
-                        myOption.font.setBold(true);
-                        }
-	
-    if (index.column() == durationColumn) 
+
+    index.model()->data(index, Qt::DisplayRole).toInt();
+    if((*myPlaylist)[index.row()]->isReading())
+    {
+        myOption.font.setBold(true);
+    }
+
+    if (index.column() == durationColumn)
     {
         int secs = index.model()->data(index, Qt::DisplayRole).toInt();
-        secs /=1000;
+        qDebug() << "delegate" << secs;
+        //secs /=1000;
         QString text = QString("%1:%2")
-                       .arg(secs / 60, 2, 10, QChar('0'))
-                       .arg(secs % 60, 2, 10, QChar('0'));
-
-
-       
+                .arg(secs / 60, 2, 10, QChar('0'))
+                .arg(secs % 60, 2, 10, QChar('0'));
 
         drawDisplay(painter, myOption, myOption.rect, text);
         drawFocus(painter, myOption, myOption.rect);
-    } 
-    else if (index.column() == YearColumn) 
-    {
-      
-      int years= index.model()->data(index, Qt::DisplayRole).toInt();
-      QString text;
-      if(years<0)
-        text = tr("Unknown");
-      if(years>=0)
-      {
-        text = QString::number(years,10);
-      }
-
-      drawDisplay(painter, myOption, myOption.rect, text);
-      drawFocus(painter, myOption, myOption.rect);
     }
-    else if (index.column() == BitrateColumn) 
+    else if (index.column() == YearColumn)
     {
-      int secs = index.model()->data(index, Qt::DisplayRole).toInt();
 
-      QString text = QString("%1 Kb/s")
-          .arg(secs / 1000, 3, 10, QChar('0'));
+        int years= index.model()->data(index, Qt::DisplayRole).toInt();
+        QString text;
+        if(years<0)
+            text = tr("Unknown");
+        if(years>=0)
+        {
+            text = QString::number(years,10);
+        }
 
-
-
-      drawDisplay(painter, myOption, myOption.rect, text);
-      drawFocus(painter, myOption, myOption.rect);
+        drawDisplay(painter, myOption, myOption.rect, text);
+        drawFocus(painter, myOption, myOption.rect);
     }
-    else if (index.column() == GenreColumn) 
+    else if (index.column() == BitrateColumn)
+    {
+        int secs = index.model()->data(index, Qt::DisplayRole).toInt();
+
+        QString text = QString("%1 Kb/s")
+                .arg(secs / 1000, 3, 10, QChar('0'));
+
+
+
+        drawDisplay(painter, myOption, myOption.rect, text);
+        drawFocus(painter, myOption, myOption.rect);
+    }
+    else if (index.column() == GenreColumn)
     {
 
-       int indexi = index.model()->data(index, Qt::DisplayRole).toInt();
-       QString text;
-       if((indexi<listgenre.size())&&(indexi>-1))
-        text = listgenre.at(indexi);
+        int indexi = index.model()->data(index, Qt::DisplayRole).toInt();
+        QString text;
+        if((indexi<listgenre.size())&&(indexi>-1))
+            text = listgenre.at(indexi);
         else
-         text = tr("None");
-       
+            text = tr("None");
 
 
- 
-       drawDisplay(painter, myOption, myOption.rect, text);
-       drawFocus(painter, myOption, myOption.rect);
+
+
+        drawDisplay(painter, myOption, myOption.rect, text);
+        drawFocus(painter, myOption, myOption.rect);
     }
     else
     {
@@ -101,22 +99,22 @@ index.model()->data(index, Qt::DisplayRole).toInt();
 }
 void TrackDelegate::setdurationColumn(int durationColumn)
 {
-  this->durationColumn = durationColumn;
+    this->durationColumn = durationColumn;
 
 }
 void TrackDelegate::setYearColumn(int YearColumn)
 {
-  this->YearColumn = YearColumn;
+    this->YearColumn = YearColumn;
 
 }
 void TrackDelegate::setBitrateColumn(int BitrateColumn)
 {
-  this->BitrateColumn = BitrateColumn;
+    this->BitrateColumn = BitrateColumn;
 
 }
 void TrackDelegate::setGenreColumn(int GenreColumn)
 {
-  this->GenreColumn=GenreColumn;
+    this->GenreColumn=GenreColumn;
 }
 
 
